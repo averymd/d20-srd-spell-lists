98dd383875471500828fe0c3a45d3de7dd34833b
Melissa Avery-Weir <melissa.avery@wellsfargo.com>
2011-09-13 16:32:32 -0400

    Initial project commit.
    Signed-off-by: Melissa Avery-Weir <melissa.avery@wellsfargo.com>

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace d20_SRD_Spell_Lists {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
